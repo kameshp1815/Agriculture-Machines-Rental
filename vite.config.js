a74f0d445c2cc58be2ea433c5c25b52371d39c0a
KAMESH <kameshkamesh3876@gmail.com>
2025-01-28 09:17:33 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Root directory where index.html is located
+  build: {
+    outDir: 'build', // Output directory for the build
+  },
 })
