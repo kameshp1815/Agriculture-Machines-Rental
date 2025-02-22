147f2e3de4b058f98b51b5a03162ea150813612c
KAMESH <168918180+kameshp1815@users.noreply.github.com>
2025-02-22 09:21:52 +0530

    Update Booking.jsx

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -22,6 +22,7 @@ const Booking = () => {
     alert(
       `Booking confirmed for ${userDetails.name}!\nMachine: ${machine.name}\nTotal Amount: Rs ${totalAmount}`
     );
+     navigate("/Catalog"); 
   };
 
   useEffect(() => {
